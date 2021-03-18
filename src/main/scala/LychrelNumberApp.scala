106e843501fce567f214faecbbec0175e067d171
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2021-03-18 22:12:27 +0100

    Change cmd-line parameter parsing; add -s

--- src/main/scala/LychrelNumberApp.scala
+++ src/main/scala/LychrelNumberApp.scala
@@ -5,9 +5,48 @@ import scala.math.BigInt
 
 
 object LychrelNumberApp extends LychrelData with App {
-  val path = searchForPalindrome(BigInt(number), steps = max_steps)
-  val result = path.head.toString
-  println(s"Number:\n${result}\nSize: ${result.length}")
+  type ParameterMap = Map[String, String]
+
+  val params = paramsToMap(Map(), args.toList)
+  if (params.contains("INVALID")) {
+    println(params("INVALID"))
+    println(help())
+  } else if (params.contains("number")) {
+    val path = searchForPalindrome(
+      BigInt(params("number")),
+      steps = params.getOrElse("steps", max_steps.toString).toInt
+    )
+    val result = path.head
+    if (params.contains("path"))
+      path.reverse.foreach(println(_))
+    else
+      println(result)
+    println(s"Size: ${result.toString.length}, steps: ${path.length}")
+  }
+  else
+    println(help())
+
+
+  @tailrec
+  def paramsToMap(map: ParameterMap, lst: List[String]): ParameterMap = {
+    lst match {
+      case Nil => map
+      case "-p" :: tail =>
+        paramsToMap(map ++ Map("path" -> ""), tail)
+      case "-s" :: value :: tail if value.matches("""^\d+$""") =>
+        paramsToMap(map ++ Map("steps" -> value), tail)
+      case value :: tail if value.matches("""^\d+$""") =>
+        paramsToMap(map ++ Map("number" -> value), tail)
+      case param =>
+        Map("INVALID" -> s"Error: Invalid parameter ${param.mkString(", ")}.\n")
+    }
+  }
+
+  def help(): String =
+    """
+      |Usage:
+      |        LychrelNumberApp <number> [-s <max_steps>] [-p]
+      |""".stripMargin
 
   @tailrec
   def searchForPalindrome(number: BigInt, path: List[BigInt] = Nil, steps: Int = 10): List[BigInt] = {
@@ -21,6 +60,6 @@ object LychrelNumberApp extends LychrelData with App {
 }
 
 trait LychrelData {
-  lazy val number = 196
-  lazy val max_steps = 5
+  lazy val max_steps = 50
+  /* HP Omen (laptop), Core i5@4.5 GHz 10th gen.: 100 000 steps ~ 19 min 59 secs */
 }
